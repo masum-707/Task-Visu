c89576fa1fc62844842158e10e4663bf3c64b901
masum-707 <mdshafikulislammasum39@gmail.com>
2026-09-21 19:16:35 +0600

    add configuration file with base URL and storage keys

---
+++ js/config.js
@@ -0,0 +1,21 @@
+// console.log("Hello from js");
+export const config = {
+  baseURL: "http://crm.test.local/",
+  pageSize: 5,
+  storageKey: {
+    token: "userToken",
+    user: "userName",
+  },
+};
+
+// http://crm.test.local/api/login
+
+/*
+
+ignorePermissions: true
+password: "123"
+username: "shible"
+*/
+
+//task
+// api/task-visu/tasks?responsible_id[]=74&is_active=true&completion_lt=100&top=40&skip=0&permission=TASKVISU_SUPERADMIN
