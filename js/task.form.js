25eba1e01fbd9a8003e77e628324effab22c2137
masum-707 <mdshafikulislammasum39@gmail.com>
2026-09-21 19:12:37 +0600

    add task form cancellation functionality with confirmation prompt

---
+++ js/task.form.js
@@ -0,0 +1,11 @@
+const formContainer = document.getElementById("form-container");
+const taskForm = document.getElementById("task-Form");
+const cancel = document.getElementById("cancel");
+cancel.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (confirm("Are you sure you want to cancel")) {
+    taskForm.reset();
+    taskForm.hidden = "true";
+  }
+  return;
+});
