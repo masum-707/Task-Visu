9863e33a592cb7847166cb190573ea03e8f57e12
masum-707 <mdshafikulislammasum39@gmail.com>
2026-09-21 12:13:19 +0600

    refactor login form submission to handle async login and improve error handling including local storage management

--- js/login.js
+++ js/login.js
@@ -1,10 +1,12 @@
-console.log("login file connected");
+import { saveSession } from "./utils.js";
+
+// console.log("login file connected");
 const usernameError = document.getElementById("username-error-message");
 const passwordError = document.getElementById("password-error-message");
 const loginBtn = document.getElementById("login-button");
 const loginForm = document.getElementById("login-form");
 
-loginForm.addEventListener("submit", (e) => {
+loginForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const userName = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value.trim();
@@ -19,8 +21,28 @@ loginForm.addEventListener("submit", (e) => {
   }
   passwordError.textContent = "";
   loginBtn.disable = true;
-  loginBtn.textContent = "Sign in....";
+  loginBtn.textContent = "Signing in....";
   try {
-    //login api
-  } catch (e) {}
+    const response = await login();
+    if (!response.ok) {
+      throw new ApiError(
+        "Wrong username or password\n Please try again",
+        response.status,
+      );
+    }
+    const result = response.json();
+    saveSession(result.token, result.username);
+    window.location.href = "tasks.html";
+  } catch (e) {
+    console.log(e.message, e.status);
+
+    const warning = document.getElementById("wrong-user");
+    warning.textContent = e.message;
+    setTimeout(() => {
+      warning.textContent = "";
+    }, 5000);
+    loginBtn.textContent = "Log in";
+    loginBtn.disable = false;
+    return;
+  }
 });
